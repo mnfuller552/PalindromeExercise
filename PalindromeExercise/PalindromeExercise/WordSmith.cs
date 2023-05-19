4f9f746d0a4950ab21d88c725991f60a946fc9a8
Matt Fuller <mnfuller552@gmail.com>
2023-05-19 16:29:52 -0400

    done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+        public bool IsPalindrome(string word)
+        {
+            
+            var reversed = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+            
+            if (reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
-
-namespace PalindromeExerciseTests
+using PalindromeExercise;
+public class UnitTest1
 {
-    public class UnitTest1
+    [Theory]
+    [InlineData("racecar", true)]
+    [InlineData("hello" , false)]
+    [InlineData("Racecar" , true)]
+    public void MyTest(string word, bool expected)
     {
-        [Fact]
-        public void Test1()
-        {
+        var tester = new WordSmith();
 
-        }
+        var actual = tester.IsPalindrome(word);
+
+        Assert.Equal(expected, actual);
+
     }
 }
